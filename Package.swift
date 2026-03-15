b48d1cfecb01323809f466207bfd4f0871e564f3
Mathieu Beaudin <macthieu@me.com>
2026-03-15 19:08:50 -0400

    build(deps): pin OrchivisteKit to v0.2.0 for canonical contracts

--- Package.swift
+++ Package.swift
@@ -8,18 +8,36 @@ let package = Package(
     ],
     products: [
         .library(name: "MuniPreclassementCore", targets: ["MuniPreclassementCore"]),
+        .library(name: "MuniPreclassementInterop", targets: ["MuniPreclassementInterop"]),
         .executable(name: "muni-preclassement-cli", targets: ["MuniPreclassementCLI"])
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-testing.git", from: "0.7.0")
+        .package(url: "https://github.com/apple/swift-testing.git", from: "0.7.0"),
+        .package(url: "https://github.com/Macthieu/OrchivisteKit.git", exact: "0.2.0")
     ],
     targets: [
         .target(name: "MuniPreclassementCore"),
-        .executableTarget(name: "MuniPreclassementCLI", dependencies: ["MuniPreclassementCore"]),
+        .target(
+            name: "MuniPreclassementInterop",
+            dependencies: [
+                "MuniPreclassementCore",
+                .product(name: "OrchivisteKitContracts", package: "OrchivisteKit")
+            ]
+        ),
+        .executableTarget(
+            name: "MuniPreclassementCLI",
+            dependencies: [
+                "MuniPreclassementInterop",
+                .product(name: "OrchivisteKitContracts", package: "OrchivisteKit"),
+                .product(name: "OrchivisteKitInterop", package: "OrchivisteKit")
+            ]
+        ),
         .testTarget(
             name: "MuniPreclassementTests",
             dependencies: [
                 "MuniPreclassementCore",
+                "MuniPreclassementInterop",
+                .product(name: "OrchivisteKitContracts", package: "OrchivisteKit"),
                 .product(name: "Testing", package: "swift-testing")
             ]
         )
